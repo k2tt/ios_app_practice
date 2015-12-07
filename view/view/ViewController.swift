68440779883383a012b22cb34c874c56dedc4095
OD <littfu@gmail.com>
2015-12-08 02:30:09 +0900

    View fin

--- view/view/ViewController.swift
+++ view/view/ViewController.swift
@@ -16,6 +16,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        let view1Frame = CGRectMake(0, 100, 320, 300)
+        let view2Frame = CGRectMake(30, 50, 80, 150)
+        let view1 = UIView(frame: view1Frame)
+        let view2 = UIView(frame: view2Frame)
+        view1.backgroundColor = UIColor.redColor()
+        view2.backgroundColor = UIColor.greenColor()
+        
+        self.view.addSubview(view1)
+        view1.addSubview(view2)
+        
+        view2.layer.borderWidth = 5
+        view2.layer.borderColor = UIColor.whiteColor().CGColor
+        view2.layer.cornerRadius = 20
+        view1.layer.shadowOpacity = 1.0
+        view1.layer.shadowOffset = CGSizeMake(5, 10)
+        
+        
         //        subView.backgroundColor = UIColor.orangeColor()
 //        label.backgroundColor = UIColor(red: 30.0/255, green: 144.0/255, blue: 255.0/255, alpha: 1.0)
 //        subView.alpha = 0.0
