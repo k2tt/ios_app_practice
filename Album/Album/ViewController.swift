fcc333cd8a7a7913847b22591a1cf2953f5ebdec
OD <littfu@gmail.com>
2015-12-19 01:27:57 +0900

    Completed

--- Album/Album/ViewController.swift
+++ Album/Album/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,8 +24,28 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        setBorderToImageView(imageView1)
+        setBorderToImageView(imageView2)
+        setBorderToImageView(imageView3)
+        setBorderToImageView(imageView4)
+        setBorderToImageView(imageView5)
+
+    }
+    
+    func setBorderToImageView (imageView : UIImageView) {
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.whiteColor().CGColor
     }
 
+    @IBAction func tapButton(sender: UIButton) {
+        imageView1.image = UIImage(named: "\(arc4random() % 30 + 1).png")
+        imageView2.image = UIImage(named: "\(arc4random() % 30 + 1).png")
+        imageView3.image = UIImage(named: "\(arc4random() % 30 + 1).png")
+        imageView4.image = UIImage(named: "\(arc4random() % 30 + 1).png")
+        imageView5.image = UIImage(named: "\(arc4random() % 30 + 1).png")
+        
+    }
 
 }
 
