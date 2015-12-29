9c88997e1e4c143cee3102150fa47d2f87a35fc2
OD <littfu@gmail.com>
2015-12-29 12:19:20 +0900

    Completed

--- text_app/text_app/ViewController.swift
+++ text_app/text_app/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var commentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        commentTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapSubmitButton(sender: UIButton) {
+        commentTextField.resignFirstResponder()
+        
+        let label = makeCommentLabel()
+
+        //ビューに追加
+        self.view.addSubview(label)
+        
+        //0.1秒毎に moveComment を実行
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "moveComment:", userInfo: label, repeats: true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        commentTextField.resignFirstResponder()
+        return true
+    }
+    
+    /**
+     * コメントラベル作成
+     * return {UILabel} 作成したラベル
+     */
+    func makeCommentLabel() -> UILabel {
+        let label = UILabel()
+        
+        //ビュー全体から、ヘッダーを抜いたY位置をランダムで取得
+        let randY = CGFloat(arc4random()) % (self.view.frame.height - 64) + 64
+        
+        //20〜40をランダムで生成
+        let fontSize = CGFloat(arc4random() % 21) + 20;
+        
+        label.text = commentTextField.text
+        label.frame.origin = CGPoint(x: self.view.frame.width, y: randY)
+        label.font = UIFont(name: "HirakakuProN-W6", size:fontSize)
+        
+        //ラベルの大きさを文字の大きさにフィットさせる
+        label.sizeToFit()
+        return label
+    }
+    
+    func moveComment(sender: NSTimer) {
+        let commentLabel = sender.userInfo as! UILabel
+        commentLabel.frame.origin.x -= commentLabel.frame.height
+    }
 
 }
 
