53191df29ee20199c1aecb80db678b33a43c3e28
OD <littfu@gmail.com>
2015-12-31 01:37:14 +0900

    First commit

---
+++ ScrollView/ScrollView/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by k2tt on 2015/12/29.
+//  Copyright © 2015年 k2tt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myScrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        myScrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height)
+        
+        //UIImageViewクラスのインスンタンスを生成
+        let myImageView = UIImageView()
+        
+        //myImageViewに画像を設定
+        myImageView.image = UIImage(named: "pug.png")
+        
+        myImageView.frame = CGRectMake(0, 0, 1000, 1000)
+        
+        //myImageViewをmyScrollViewのビューに配置
+        myScrollView.addSubview(myImageView)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
