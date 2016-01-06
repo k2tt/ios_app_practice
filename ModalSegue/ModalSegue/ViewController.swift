5f8a4123af9ce0d6d1eb5a3f91428be473d708d3
OD <littfu@gmail.com>
2016-01-07 01:46:36 +0900

    First commit

---
+++ ModalSegue/ModalSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModalSegue
+//
+//  Created by k2tt on 2016/01/07.
+//  Copyright © 2016年 k2tt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
