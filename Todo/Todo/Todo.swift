5698dcb2a3f8dec383a44bfe2d1a74f4e79d9ce6
OD <littfu@gmail.com>
2016-02-04 02:36:46 +0900

    MVC構造へ

---
+++ Todo/Todo/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  Todo
+//
+//  Created by k2tt on 2016/02/04.
+//  Copyright © 2016年 k2tt. All rights reserved.
+//
+
+import UIKit
+
+class Todo: NSObject {
+    var title = ""
+}
